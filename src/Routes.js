db679a1af99c5a37f1904b73bcece95f6b934ab6
songsuho <ridou99@gmail.com>
2020-09-17 12:52:07 +0900

    add: 테슬라 중간 커밋파일수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import side1 from "./Main/side1";
-import Info from "./Main/Info";
+import Nav from "./Main/Nav";
 
 function Routes() {
   return(
     <Router>
       <Switch>
-        <Route exact="/" path="/" component={side1} />
-        <Route exact="/info" path="/info" component={Info} />
+        <Route exact="/" component={Nav} />
       </Switch>
     </Router>
   )
